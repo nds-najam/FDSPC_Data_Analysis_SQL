a5ee366f5c95fb02998fd82eb45b200cb79a67f5
Sheikh Najam <64066796+nds-najam@users.noreply.github.com>
2024-07-17 11:06:18 +0530

    Update Week4.sql (Find food preferences of customers)

--- Week4.sql
+++ Week4.sql
@@ -21,3 +21,17 @@ SELECT customers.name, products.product_name, sales.quantity
 FROM customers
 FULL JOIN sales ON customers.id = sales.customer_id
 FULL JOIN products ON sales.product_id = products.id;
+
+
+SELECT t2.food_type_new, SUM(t1.quantity) AS item_quantity
+FROM orders_items t1
+LEFT JOIN (
+	SELECT item_id,
+		CASE 
+			WHEN food_type LIKE 'veg%' THEN 'veg'
+			ELSE 'non-veg'
+		END AS food_type_new
+	FROM food_items
+			) t2 ON t1.item_id = t2.item_id
+GROUP BY t2.food_type_new;
+
