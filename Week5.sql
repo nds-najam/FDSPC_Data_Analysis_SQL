2b9d222a64649d562107448ce60acdd30955191d
Sheikh Najam <64066796+nds-najam@users.noreply.github.com>
2024-07-27 07:17:21 +0530

    Update Week5.sql

--- Week5.sql
+++ Week5.sql
@@ -1,8 +1,17 @@
 create database competitor_analysis;
 use competitor_analysis;
-create table vendors (company_id varchar(5) primary_key, name char(255), website varchar(255), num_users int, num_cities int);
+CREATE table vendors (company_id varchar(5) primary_key, name char(255), website varchar(255), num_users int, num_cities int);
 
 INSERT INTO competitor_analysis.vendors (company_id, Name, website, num_users, num_cities)
 VALUES
 ('C1','FoodBae','https://www.foodbae.com',8540,15),
 ('C2','Yumzo','https://www.yumzo.com',9670,18);
+
+CREATE table vendors_metrics
+(
+id int auto_increment primary key,
+company_id varchar(5),
+month int,
+num_orders int,
+num_sales float,
+foreign key(company_id) references vendors(company_id));
